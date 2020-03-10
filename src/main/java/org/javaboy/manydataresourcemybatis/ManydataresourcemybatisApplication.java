c5a333f92dae2cfd6bcb6e8323f5504646102f58
unknown <myarctic@163.com>
2020-03-10 21:28:13 +0800

    初始化

---
+++ src/main/java/org/javaboy/manydataresourcemybatis/ManydataresourcemybatisApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.manydataresourcemybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManydataresourcemybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManydataresourcemybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaboy/manydataresourcemybatis/ManydataresourcemybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.manydataresourcemybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManydataresourcemybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
